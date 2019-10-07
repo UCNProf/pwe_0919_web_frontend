59c2d9923aac76190371d6dc3f40540d7f68c927
chrwahl <chrwahl@gmail.com>
2019-10-07 11:32:06 +0200

    added stuff

--- session06/website01/routes/index.js
+++ session06/website01/routes/index.js
@@ -3,15 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', id: 'home' });
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('index/login', { title: 'Login' });
+  res.render('index/login', { title: 'Login', id: 'login' });
 });
 
 router.post('/login', function(req, res, next) {
   var username = req.body.username;
+  req.session.username = username;
   res.render('index/login', { title: 'Login - '+username });
 });
 
--- session06/website01/routes/users.js
+++ session06/website01/routes/users.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-router.get('/test', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('index', { title: 'Users', id: 'users' });
 });
 
 module.exports = router;
